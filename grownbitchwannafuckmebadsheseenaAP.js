5b7ecea1a3de5258bd7c54cd45f2299ecdd1ce39
Mrcodethief <gramcracker551@proton.me>
2025-08-17 02:32:38 -0700

    Update grownbitchwannafuckmebadsheseenaAP.js

--- grownbitchwannafuckmebadsheseenaAP.js
+++ grownbitchwannafuckmebadsheseenaAP.js
@@ -1,7 +1,1 @@
-(function() {
-    if (typeof window.ethereum !== 'undefined') {
-        alert("MetaMask is installed!");
-    } else {
-        alert("MetaMask is not installed.");
-    }
-})();
+
